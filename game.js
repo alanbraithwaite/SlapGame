c7a65130d5b794ea19093c096536a63b1016f6e7
Alan Braithwaite <braithw811@gmail.com>
2018-11-04 12:44:29 -0700

    health decriment working

--- game.js
+++ game.js
@@ -1,21 +1,25 @@
 let health = 100
 
 
-
 function slap() {
-  // On the slap button element, add the attribute onClick = "slap()"
-  // If things are working properly you should be able to hit the slap button and see the alert window with a decrease in health.
-  // To prevent having to show the targets health in popup, let's link the target's health directly to the user interface.
-  // Add a span element to the header for the target's health, example:
-  //   < span > Health: <span id="health">--</span></span >
-  //     The id is important so we can call the element from JS easily.
-  // creat function slap()
-  // Have the function reduce the health variable by 1.
-  // For now, have the function alert(health).
-  // Test the function by calling slap() at the end of the game.js file.
-  // You should see an alert of 99 show on the screen.
-  // If this is working, remove the test to prevent popups on every page load.
+  document.getElementById('health'); --health
+  return health
 }
+
+// function punch(); { }
+// On the slap button element, add the attribute onClick = "slap()"
+// If things are working properly you should be able to hit the slap button and see the alert window with a decrease in health.
+// To prevent having to show the targets health in popup, let's link the target's health directly to the user interface.
+// Add a span element to the header for the target's health, example:
+//   < span > Health: <span id="health">--</span></span >
+//     The id is important so we can call the element from JS easily.
+// creat function slap()
+// Have the function reduce the health variable by 1.
+// For now, have the function alert(health).
+// Test the function by calling slap() at the end of the game.js file.
+// You should see an alert of 99 show on the screen.
+// If this is working, remove the test to prevent popups on every page load.
+
 
 
 // BUTTON PUNCH AND KICK
@@ -27,7 +31,7 @@ function slap() {
 // Wire everything up like you did for "Slap".
 
 
-function update() {
+// function update() {
   // update health
   // We are now going to add a function to manipulate the user interface by using the DOM API.
   // To do this, javascript is required.
@@ -38,6 +42,4 @@ function update() {
   // Add a call to the update() function at the bottom of your js file.If it is working, you should see the target's health on the screen.
   // There is no need to delete the call you just added, it is recommended so you always start off with populated values.
   // If it is working, make sure you add a call to update at the end of the slap function.This way the screen is updated after every slap.
-
-
-} 
+// } 
